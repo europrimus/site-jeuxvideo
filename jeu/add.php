5de59eb78ff91ca4502e6abcf2bac3e89348a3b7
Jean P <jean@pruliere.fr>
2018-04-20 12:38:16 +0200

    feat #5 : ajout d'un formulaire d'ajout

---
+++ jeu/add.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once('../inc/connecteur.php');
+
+if (isset($_POST['confirmation'])) {
+	if (isset($_POST['devedi'])) $_POST['developpeur'] = $_POST['editeur'];
+	$stmt = $pdo->prepare("INSERT INTO jeu (titre, annee, developpeur, editeur) VALUES (:titre, :annee, :developpeur, :editeur)");
+	$stmt->execute([
+		'titre' => $_POST['titre'],
+		'annee' => $_POST['annee'],
+		'developpeur' => $_POST['developpeur'],
+		'editeur' => $_POST['editeur']
+	]);
+	header('Location: view.php?id='.$pdo->lastInsertId());
+	exit;
+}
+
+require_once('../tpl/header.tpl');
+
+?>
+<form method="POST">
+	<dl>
+		<dt>Titre</dt>
+		<dd><input type="text" name="titre"></dd>
+		<dt>Année</dt>
+		<dd><input type="number" name="annee"></dd>
+		<dt>Éditeur</dt>
+		<dd><input type="text" name="editeur"></dd>
+		<dt>Développeur (<input type="checkbox" name="devedi"> = éditeur)</dt>
+		<dd><input type="text" name="developpeur"></dd>
+	</dl>
+	<input type="submit" name="confirmation" value="Ajouter">
+</form>
+<a href="list.php">Retourner à la liste</a>
+<?php
+
+require_once('../tpl/footer.tpl');
