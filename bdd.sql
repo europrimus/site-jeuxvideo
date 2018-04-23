9780dc51313088213f3c5510c144167c75ba3928
Jean P <jean@pruliere.fr>
2018-04-23 14:55:22 +0200

    v2 de la bdd

--- bdd.sql
+++ bdd.sql
@@ -7,3 +7,34 @@ CREATE TABLE jeu (
 	developpeur varchar(50) NOT NULL,
 	annee date NOT NULL
 );
+
+-- r2
+
+CREATE TABLE console (
+	id serial PRIMARY KEY,
+	nom varchar(50) UNIQUE NOT NULL,
+	sigle varchar(8) UNIQUE NOT NULL,
+	puissance int NOT NULL CHECK (round(log(2, puissance)) = log(2, puissance)),
+	datesortie date NOT NULL,
+	constructeur varchar(50) NOT NULL
+);
+
+CREATE TYPE typesortie AS ENUM ('initiale', 'portage', 'remaster', 'remake');
+
+CREATE TABLE version (
+	id serial PRIMARY KEY,
+	datesortie date NOT NULL,
+	typesortie typesortie NOT NULL DEFAULT 'initiale'::typesortie,
+	idjeu int NOT NULL REFERENCES jeu(id),
+	idconsole int NOT NULL REFERENCES console(id)
+);
+
+INSERT INTO console (nom, sigle, puissance, datesortie, constructeur)
+VALUES ('Nintendo', 'NES', 8, '1985-07-15', 'Nintendo'),
+('Super Nintendo', 'SNES', 16, '1990-11-21', 'Nintendo'),
+('PlayStation', 'PSX', 32, '1994-12-03', 'Sony');
+
+INSERT INTO version(datesortie, idjeu, idconsole)
+SELECT annee, id, 1 FROM jeu;
+
+ALTER TABLE jeu DROP COLUMN annee;
