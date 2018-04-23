24754dce8f252eef17df2046dd1c245aad9d3d1c
Jean P <jean@pruliere.fr>
2018-04-23 15:31:01 +0200

    Ajout de la class Console et du listing

---
+++ classes/console.class.php
@@ -0,0 +1,67 @@
+<?php
+
+class Console {
+
+	private $id;
+	private $nom;
+	private $sigle;
+	private $puissance;
+	private $datesortie;
+	private $constructeur;
+
+	public function getId() {
+		return $this->id;
+	}
+
+	public function setId($id) {
+		if (is_null($this->id)) {
+			$this->id = $id;
+		}
+		return $this;
+	}
+
+	public function getNom() {
+		return $this->nom;
+	}
+
+	public function setNom($nom) {
+		$this->nom = $nom;
+		return $this;
+	}
+
+	public function getSigle() {
+		return $this->sigle;
+	}
+
+	public function setSigle($sigle) {
+		$this->sigle = $sigle;
+		return $this;
+	}
+
+	public function getPuissance() {
+		return $this->puissance;
+	}
+
+	public function setPuissance($puissance) {
+		$this->puissance = $puissance;
+		return $this;
+	}
+
+	public function getDatesortie() {
+		return $this->datesortie;
+	}
+
+	public function setDatesortie($datesortie) {
+		$this->datesortie = $datesortie;
+		return $this;
+	}
+
+	public function getConstructeur() {
+		return $this->constructeur;
+	}
+
+	public function setConstructeur($constructeur) {
+		$this->constructeur = $constructeur;
+		return $this;
+	}
+}
---
+++ console/list.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once('../inc/connecteur.php');
+
+require_once('../classes/console.class.php');
+
+$res = $pdo->query("SELECT * FROM console")->fetchAll(PDO::FETCH_ASSOC);
+$consoles = [];
+foreach ($res as $console) {
+	$c = new Console();
+	$c->setId($console['id'])
+		->setNom($console['nom'])
+		->setSigle($console['sigle'])
+		->setPuissance($console['puissance'])
+		->setDatesortie($console['datesortie'])
+		->setConstructeur($console['constructeur']);
+	$consoles[] = $c;
+}
+
+require_once('../tpl/header.tpl');
+
+?>
+<a href="add.php">Ajouter une console</a>
+<table>
+	<thead>
+		<tr>
+			<th>Nom (sigle)</th>
+			<th>Puissance</th>
+			<th>Constructeur / Date de sortie</th>
+			<th>Actions</th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php foreach ($consoles as $console): ?>
+		<tr>
+			<td><?=$console->getNom()." (".$console->getSigle().")"?></td>
+			<td><?=$console->getPuissance()?></td>
+			<td><?=$console->getConstructeur()." / ".$console->getDatesortie()?></td>
+			<td><a href="view.php?id=<?=$console->getId()?>">Voir</a> <a href="edit.php?id=<?=$console->getId()?>">Modifier</a> <a href="delete.php?id=<?=$console->getId()?>">Supprimer</a></td>
+		</tr>
+		<?php endforeach; ?>
+	</tbody>
+</table>
+<?php
+
+require_once('../tpl/footer.tpl');
