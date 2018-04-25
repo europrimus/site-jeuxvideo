49f2955c8d4384002ef6aa1ea45e58f5372e0b0d
Jean P <jean@pruliere.fr>
2018-04-25 16:43:20 +0200

    coquille grammaire dans la suppression de version

--- version/delete.php
+++ version/delete.php
@@ -32,7 +32,7 @@ $j->setId($jeu['id'])
 require_once('../tpl/header.tpl');
 
 ?>
-<h3>Voulez-vous vraiment supprimer la version de <?=$j->getJeu()?> sur <?=$j->getConsole()?></h3>
+<h3>Voulez-vous vraiment supprimer la version de <?=$j->getJeu()?> sur <?=$j->getConsole()?> ?</h3>
 <ul>
 	<li><form method="post"><button type="submit" name="confirmation">Oui, je confirme</button></form></li>
 	<li><a href="edit.php?id=<?=$j->getId()?>"><button type="button">Non, je veux juste le modifier</button></a></li>
