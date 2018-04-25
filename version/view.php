42efc9425a98bc9eea9e338e0edcf42f56388563
Jean P <jean@pruliere.fr>
2018-04-25 09:32:32 +0200

    Ajout de la vue détaillée d'une version

---
+++ version/view.php
@@ -0,0 +1,47 @@
+<?php
+
+if (!isset($_GET['id'])) {
+	header('Location:list.php');
+	exit;
+}
+
+require_once('../inc/connecteur.php');
+
+require_once('../classes/version.class.php');
+
+$stmt = $pdo->prepare("SELECT version.*, jeu.titre titrejeu, console.nom nomconsole
+	FROM version
+	JOIN jeu ON jeu.id = version.idjeu
+	JOIN console ON console.id = version.idconsole
+	WHERE version.id = :id");
+$stmt->execute(['id' => $_GET['id']]);
+$jeu = $stmt->fetch(PDO::FETCH_ASSOC);
+
+$j = new Version();
+$j->setId($jeu['id'])
+	->setJeu($jeu['titrejeu'])
+	->setConsole($jeu['nomconsole'])
+	->setDatesortie($jeu['datesortie'])
+	->setTypesortie($jeu['typesortie']);
+
+require_once('../tpl/header.tpl');
+
+?>
+<dl>
+	<dt>Jeu</dt>
+	<dd><?=$j->getJeu()?></dd>
+	<dt>Console</dt>
+	<dd><?=$j->getConsole()?></dd>
+	<dt>Date de sortie</dt>
+	<dd><?=$j->getDatesortie(true)?></dd>
+	<dt>Type</dt>
+	<dd><?=$j->getTypesortie()?></dd>
+</dl>
+<ul>
+	<li><a href="edit.php?id=<?=$j->getId()?>">Modifier</a></li>
+	<li><a href="delete.php?id=<?=$j->getId()?>">Supprimer</a></li>
+	<li><a href="list.php">Retourner à la liste</a></li>
+</ul>
+<?php
+
+require_once('../tpl/footer.tpl');
